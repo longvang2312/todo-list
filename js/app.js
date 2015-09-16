e7a1f210f5b9ef4d1ec8e0551dba9db11573655e
Ken Ho <ken.ho170381@gmail.com>
2015-09-16 15:02:06 +0700

    finish

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
-var todoApp = angular.module('todoApp',['firebase']);
+var todoApp = angular.module('todoApp', ['firebase']);
 
-todoApp.factory("todoFact",function($firebaseArray) {
+todoApp.factory("todoFact", function ($firebaseArray) {
 
-     var ref = new Firebase("https://radiant-inferno-6106.firebaseio.com/Todo");
+  var ref = new Firebase("https://radiant-inferno-6106.firebaseio.com/Todo");
 
 
-     return $firebaseArray(ref);
- });
+  return $firebaseArray(ref);
+});
 
-todoApp.controller("todoCtrl",function($scope,todoFact){
-		//$scope.todoList=[];
-		$scope.filter = {};
-		$scope.myData = todoFact;
-		console.log($scope.myData);
+todoApp.controller("todoCtrl", function ($scope, todoFact) {
+  //$scope.todoList=[];
+  $scope.filter = {};
+  $scope.myData = todoFact;
 //		$scope.todoClear = function(){
 //        			var newTodo = [];
 //        			angular.forEach($scope.todoList,
@@ -22,21 +21,20 @@ todoApp.controller("todoCtrl",function($scope,todoFact){
 //        				});
 //        			$scope.todoList = newTodo;
 //        		};
-		$scope.todoClear = function(){
-			angular.forEach($scope.myData,
-				function(todo){
-				if(todo.complete){
-				$scope.myData.$remove(todo);
-				}
-			});
+  $scope.todoClear = function () {
+    angular.forEach($scope.myData,
+      function (todo) {
+        if (todo.complete) {
+          $scope.myData.$remove(todo);
+        }
+      });
 
-		};
-		$scope.todoEnterSave = function(){
-			if(event.keyCode == 13)
-			{ 
-					$scope.todoAdd();
-			}
-		};
+  };
+  $scope.todoEnterSave = function () {
+    if (event.keyCode == 13) {
+      $scope.todoAdd();
+    }
+  };
 //		$scope.todoAdd = function(){
 //                			if($scope.todoText)
 //                			{
@@ -44,39 +42,41 @@ todoApp.controller("todoCtrl",function($scope,todoFact){
 //                					$scope.todoText='';
 //                			}
 //                		};
-		$scope.todoAdd = function(){
-        			if($scope.todoText)
-        			{
-        					$scope.myData.$add({text:$scope.todoText,complete:false});
-        					$scope.todoText='';
-        			}
-        		};
+  $scope.todoAdd = function () {
+    if ($scope.todoText) {
+      $scope.myData.$add({text: $scope.todoText, complete: false});
+      $scope.todoText = '';
+    }
+  };
 
 //		$scope.todoCheckBoxAll=function(){
 //        			angular.forEach($scope.todoList,function(todo){
 //        			todo.complete=$scope.CheckAll;
 //        			});
 //        		};
-		$scope.todoCheckBoxAll=function(){
-			angular.forEach($scope.myData,function(todo){
-			todo.complete=$scope.CheckAll;
-			});
-		};
-		$scope.toggleEditMode= function(){
-			$(event.target).closest('li').toggleClass('editing');
-			
-		};
-		$scope.todoOnEnter=function(todo){
-				$scope.toggleEditMode();
-		};
+  $scope.todoCheckBoxAll = function () {
+    angular.forEach($scope.myData, function (todo) {
+      todo.complete = $scope.CheckAll;
+    });
+  };
+  $scope.toggleEditMode = function () {
+    $(event.target).closest('li').toggleClass('editing');
 
-		$scope.remaining = function(){
-        			var count = 0;
-        			angular.forEach($scope.myData,function(todo){
-        				 count += todo.complete ? 0 : 1; // todo.complete true hoặc false
-        				});
-        			return count;
-        		};
+  };
+  $scope.todoOnEnter = function (todo) {
+    if (event.keyCode == 13 && todo.text) {
+      $scope.toggleEditMode();
+
+    }
+  };
+
+  $scope.remaining = function () {
+    var count = 0;
+    angular.forEach($scope.myData, function (todo) {
+      count += todo.complete ? 0 : 1; // todo.complete true hoặc false
+    });
+    return count;
+  };
 //		$scope.remaining = function(){
 //			var count = 0;
 //			angular.forEach($scope.todoList,function(todo){
@@ -84,12 +84,12 @@ todoApp.controller("todoCtrl",function($scope,todoFact){
 //				});
 //			return count;
 //		};
-		$scope.todoTotal= function(){
-			return $scope.myData.length;
-		}
+  $scope.todoTotal = function () {
+    return $scope.myData.length;
+  }
 //		$scope.todoTotal= function(){
 //			return $scope.todoList.length;
 //		}
-		
-					
+
+
 });
